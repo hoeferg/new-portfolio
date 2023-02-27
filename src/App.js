a044d31ae13610c17ad26b1614753ba5478c55e9
Gayle Hoefer <gaylehoefer@gmail.com>
2023-02-27 14:12:07 -0700

    Header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./Header/Header.js"
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Header() {
     return (
         <>
-
+<h1>Gayle Hoefer</h1>
+<h2>Full Stack Web Developer</h2>
+<nav>
+    <a>Home</a>
+    <a>About Me</a>
+    <a>Skills</a>
+    <a>Contact</a>
+</nav>
         </>
     )
 }
