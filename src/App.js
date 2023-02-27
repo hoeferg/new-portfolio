2c9f959cab6794620477f8749bd41f1bb626d52d
Gayle Hoefer <gaylehoefer@gmail.com>
2023-02-27 14:15:26 -0700

    About Gayle Page Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Header/Header.js"
+import About from "./AboutMe/About.js"
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
