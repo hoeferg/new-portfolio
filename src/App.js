2af872a7ac1ae44a0345070664800f9b13fec938
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-01 12:48:11 -0700

    Added new photos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { useRef } from 'react';
 import './App.css';
-import Header from "./components/Header/Header.js";
-import Home from "./Pages/Home/Home.js";
-import About from "./Pages/AboutMe/About.js";
-import Skills from "./Pages/Skills/Skills.js";
-import Contact from "./Pages/Contact/Contact.js";
-import Footer from "./components/Footer/Footer.js";
+// import Header from "./components/Header/Header.js";
+// import Home from "./Pages/Home/Home.js";
+// import About from "./Pages/AboutMe/About.js";
+// import Skills from "./Pages/Skills/Skills.js";
+// import Contact from "./Pages/Contact/Contact.js";
+// import Footer from "./components/Footer/Footer.js";
+
+
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import music from "./photos/music.jpg";
-// import clarient from "./photos/clarient.jpg";
-// import piano from "./photos/piano.jpg";
-import sax from "./photos/sax.jpg";
+
 
 export default function App() {
   const ref = useRef();
 
   return (
     <div className="App">
+      {/* header */}
       <Parallax pages={4} ref={ref}>
-      <Header />
+
+        {/* Home */}
         <ParallaxLayer
           offset={0}
           speed={1}
@@ -28,7 +29,9 @@ export default function App() {
             backgroundImage: `url(${sax})`,
             backgroundSize: 'cover',
           }}
-        />
+        > </ParallaxLayer>
+
+        {/* projects */}
         <ParallaxLayer
           offset={1}
           speed={1}
@@ -38,31 +41,22 @@ export default function App() {
             backgroundSize: 'cover',
           }}
         ></ParallaxLayer>
+
+        {/* skills */}
         <ParallaxLayer
           sticky={{ start: 0.9, end: 2.5 }}
           style={{ textAlign: 'center' }}
         >
-          <Home/>
         </ParallaxLayer>
 
+        {/* Contact Me */}
         <ParallaxLayer
           offset={0.2}
           speed={0.05}
-          onClick={() => ref.current.scrollTo(3)}
+          onClick={() => ref.current.scrollTo(2)}
         >
-          
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={3}
-          speed={2}
-          onClick={() => ref.current.scrollTo(0)}
-        >
-
-          <About />
-        <Skills />
-        <Contact />
-        <Footer />
-        </ParallaxLayer>
+      
       </Parallax>
     </div>
   );
