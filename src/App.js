9da6f10542ad04fa8766a9ad8ea302b9f4d5105c
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-01 13:31:54 -0700

    Imported parts of old portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import './App.css';
 // import Header from "./components/Header/Header.js";
 // import Home from "./Pages/Home/Home.js";
@@ -8,56 +8,11 @@ import './App.css';
 // import Footer from "./components/Footer/Footer.js";
 
 
-
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-
-
 export default function App() {
-  const ref = useRef();
+  // const ref = useRef();
 
   return (
-    <div className="App">
-      {/* header */}
-      <Parallax pages={4} ref={ref}>
-
-        {/* Home */}
-        <ParallaxLayer
-          offset={0}
-          speed={1}
-          factor={2}
-          style={{
-            backgroundImage: `url(${sax})`,
-            backgroundSize: 'cover',
-          }}
-        > </ParallaxLayer>
-
-        {/* projects */}
-        <ParallaxLayer
-          offset={1}
-          speed={1}
-          factor={4}
-          style={{
-            backgroundImage: `url(${music})`,
-            backgroundSize: 'cover',
-          }}
-        ></ParallaxLayer>
-
-        {/* skills */}
-        <ParallaxLayer
-          sticky={{ start: 0.9, end: 2.5 }}
-          style={{ textAlign: 'center' }}
-        >
-        </ParallaxLayer>
-
-        {/* Contact Me */}
-        <ParallaxLayer
-          offset={0.2}
-          speed={0.05}
-          onClick={() => ref.current.scrollTo(2)}
-        >
-        </ParallaxLayer>
-      
-      </Parallax>
+    <div>
     </div>
   );
 }
